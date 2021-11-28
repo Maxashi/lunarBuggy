1e7ce2fce1ff866375bac52b4a76c2661c47b455
Max <31883665+Maxashi@users.noreply.github.com>
2021-11-28 14:27:20 +0100

    first orbitCam box

--- httpdocs/js/mainScript.js
+++ httpdocs/js/mainScript.js
@@ -1,21 +1,26 @@
-window.addEventListener("DOMContentLoaded", function () {
-	var canvas = document.getElementById("canvas");
-	var engine = new BABYLON.Engine(canvas, true);
+const canvas = document.getElementById("canvas"); // Get the canvas element
+const engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
 
-	var createScene = function () {
-		const scene = new BABYLON.Scene(engine);
-		const camera = new BABYLON.ArcRotateCamera("camera",
-			-Math.PI / 2, Math.PI / 2.5, 3,
-			new BABYLON.Vector3(0, 0, 0));
-		camera.attachControl(canvas, true);
-		const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0));
+var createScene = function () {
+	const scene = new BABYLON.Scene(engine);
+	const camera = new BABYLON.ArcRotateCamera("camera",
+		-Math.PI / 2, Math.PI / 2.5, 3,
+		new BABYLON.Vector3(0, 0, 0));
+	camera.attachControl(canvas, true);
+	const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0));
 
-		const box = BABYLON.MeshBuilder.CreateBox("box", {});
+	const box = BABYLON.MeshBuilder.CreateBox("box", {});
 
-		return scene;
-	}
-	var scene = createScene();
-	engine.runRenderLoop(function () {
-		scene.render();
-	});
+	return scene;
+}
+
+const scene = createScene();
+// Register a render loop to repeatedly render the scene
+engine.runRenderLoop(function () {
+	scene.render();
+});
+
+// Watch for browser/canvas resize events
+window.addEventListener("resize", function () {
+	engine.resize();
 });
