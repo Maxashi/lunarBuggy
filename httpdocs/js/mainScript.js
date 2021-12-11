b44a6bea3f24c72476e4f3734e4e910f7a893acd
Max <31883665+Maxashi@users.noreply.github.com>
2021-12-11 17:01:41 +0100

    removed unused scene function

--- httpdocs/js/mainScript.js
+++ httpdocs/js/mainScript.js
@@ -1,22 +1,11 @@
-import * as createBallScene from "./js/ball.js";
+import ballScene, { createBallScene } from "/js/ball.js";
+
 const canvas = document.getElementById("canvas"); // Get the canvas element
 const engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
 
-var createScene = function () {
-	const scene = new BABYLON.Scene(engine);
-	const camera = new BABYLON.ArcRotateCamera("camera",
-		-Math.PI / 2, Math.PI / 2.5, 3,
-		new BABYLON.Vector3(0, 0, 0));
-	camera.attachControl(canvas, true);
-	const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0));
-
-	const box = BABYLON.MeshBuilder.CreateBox("box", {});
-
-	return scene;
-}
-
-const scene = createBallScene();
 // Register a render loop to repeatedly render the scene
+const scene = createBallScene(engine);
+
 engine.runRenderLoop(function () {
 	scene.render();
 });
