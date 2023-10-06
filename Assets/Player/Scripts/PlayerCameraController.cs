fa9e05f7301e6aeeb2c2191362c439586b3f7799
Maxashi <info@max-gerdes.de>
2023-10-06 21:51:17 +0200

    Basic camera controls

--- Assets/Player/Scripts/PlayerCameraController.cs
+++ Assets/Player/Scripts/PlayerCameraController.cs
@@ -1,29 +1,67 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerCameraController : MonoBehaviour
 {
     [SerializeField] private Transform playerTransform;
-    [SerializeField] private PlayerVehicleController PlayerVehiceController;
+    [SerializeField] private PlayerVehicleController playerVehiceController;
     [SerializeField] private float minDistance = 5f;
     [SerializeField] private float maxDistance = 15f;
     [SerializeField] private float zoomSpeed = 5f;
-    [SerializeField] private float rotationSpeed = 5f;
+    [SerializeField] private float rotationSpeedY = 5f;
+ [SerializeField]  public float rotationSpeedX = 5f;
 
-    private Vector3 offset;
+    /// <summary>
+    /// Direction from target to camera.
+    /// </summary>
+    private Vector3 camDirection;
+
+    private float camDistance;
+    [SerializeField] private Vector3 lastPlayerPosition;
+  
+    [SerializeField, Range(-80f, 80f)]
+    private float minYRotation = -80f;
+
+    [SerializeField, Range(-80f, 80f)]
+    private float maxYRotation = 80f;
+
+
     private float currentRotationX = 0f;
+    private float currentRotationY = 0f;
 
     void Start()
     {
         CheckForPlayer();
 
-      
-
-        offset = transform.position - playerTransform.position;
+        var delta = transform.position - playerTransform.position;
+        camDistance = delta.magnitude;
+        camDirection = delta.normalized;
     }
+void Update()
+{
+    if (playerTransform == null)
+        return;
 
+    float mouseX = Input.GetAxis("Mouse X") * rotationSpeedX;
+    float mouseY = Input.GetAxis("Mouse Y") * rotationSpeedY;
+
+    // Incrementally update the rotation angles
+    horizontalRotation += mouseX;
+    verticalRotation = Mathf.Clamp(verticalRotation - mouseY, minYRotation, maxYRotation);
+
+    // Create a quaternion based on the rotation angles
+    Quaternion rotation = Quaternion.Euler(verticalRotation, horizontalRotation, 0);
+
+    // Use the rotation to set the camera direction
+    Vector3 camDirection = rotation * Vector3.forward;
+
+    // Update camera position based on player position and distance
+    transform.position = playerTransform.position + camDirection * camDistance;
+    transform.LookAt(playerTransform.position);
+}
     private void CheckForPlayer()
     {
-
         playerTransform = GameObject.FindWithTag("Player").transform;
 
         if (playerTransform == null)
@@ -31,27 +69,21 @@ public class PlayerCameraController : MonoBehaviour
             Debug.LogError("Player reference not set in RacingCameraController!");
             return;
         }
+
+        playerVehiceController = playerTransform.GetComponent<PlayerVehicleController>();
     }
 
-    void Update()
+    private void OnDrawGizmos()
     {
-        if (playerTransform == null)
-            return;
+        if (playerTransform)
+        {
+            var directionEndPoint = playerTransform.position + camDirection;
+            Gizmos.DrawLine(playerTransform.position, directionEndPoint);
+        }
+    }
 
-        // Adjust camera distance based on player speed
-        float targetDistance = Mathf.Lerp(minDistance, maxDistance, 0.5f);
-        offset.z = Mathf.Lerp(offset.z, -targetDistance, Time.deltaTime * zoomSpeed);
-
-        // Rotate the camera based on mouse input
-        float mouseX = Input.GetAxis("Mouse X") * rotationSpeed;
-        currentRotationX -= Input.GetAxis("Mouse Y") * rotationSpeed;
-        //currentRotationX = Mathf.Clamp(currentRotationX, -20f, 20f);
-
-        Quaternion rotation = Quaternion.Euler(currentRotationX, mouseX, 0);
-        offset = rotation * offset;
-
-        // Update camera position based on player position
-        transform.position = playerTransform.position + offset;
-        transform.rotation = rotation;
+    void OnValidate()
+    {
+        CheckForPlayer();
     }
 }
