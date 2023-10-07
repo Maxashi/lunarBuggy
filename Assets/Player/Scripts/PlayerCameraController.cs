c17e3d729ceae9fd5c24d109d36f001e0a9c2329
Maxashi <info@max-gerdes.de>
2023-10-07 16:32:11 +0200

    Refactored Cam controls

--- Assets/Player/Scripts/PlayerCameraController.cs
+++ Assets/Player/Scripts/PlayerCameraController.cs
@@ -4,31 +4,34 @@ using UnityEngine;
 
 public class PlayerCameraController : MonoBehaviour
 {
+    #region PlayerTransform
+
     [SerializeField] private Transform playerTransform;
     [SerializeField] private PlayerVehicleController playerVehiceController;
+    [SerializeField] private Vector3 lastPlayerPosition;
+    #endregion
+
+    #region  Camera Controls
     [SerializeField] private float minDistance = 5f;
     [SerializeField] private float maxDistance = 15f;
     [SerializeField] private float zoomSpeed = 5f;
-    [SerializeField] private float rotationSpeedY = 5f;
- [SerializeField]  public float rotationSpeedX = 5f;
+    [SerializeField, Range(0f, 2f)] private float rotationSpeedY = 1f;
+    [SerializeField, Range(0f, 2f)] public float rotationSpeedX = 1f;
 
     /// <summary>
     /// Direction from target to camera.
     /// </summary>
-    private Vector3 camDirection;
+    private Vector3 desiredCamDirection;
+    private Vector3 desiredCamPosition;
 
     private float camDistance;
-    [SerializeField] private Vector3 lastPlayerPosition;
-  
-    [SerializeField, Range(-80f, 80f)]
-    private float minYRotation = -80f;
 
-    [SerializeField, Range(-80f, 80f)]
-    private float maxYRotation = 80f;
+    [SerializeField, Range(-88f, 0f)] private float minYRotation = -80f;
+    [SerializeField, Range(45f, 80f)] private float maxYRotation = 88f;
 
-
-    private float currentRotationX = 0f;
-    private float currentRotationY = 0f;
+    private float currentRotationX  ;
+    private float currentRotationY ; //Start behind the player
+    #endregion
 
     void Start()
     {
@@ -36,30 +39,38 @@ public class PlayerCameraController : MonoBehaviour
 
         var delta = transform.position - playerTransform.position;
         camDistance = delta.magnitude;
-        camDirection = delta.normalized;
+        desiredCamDirection = delta.normalized;
     }
-void Update()
-{
-    if (playerTransform == null)
-        return;
 
-    float mouseX = Input.GetAxis("Mouse X") * rotationSpeedX;
-    float mouseY = Input.GetAxis("Mouse Y") * rotationSpeedY;
+    void Update()
+    {
+        if (playerTransform == null)
+            return;
 
-    // Incrementally update the rotation angles
-    horizontalRotation += mouseX;
-    verticalRotation = Mathf.Clamp(verticalRotation - mouseY, minYRotation, maxYRotation);
+        transform.position = GetDesiredCamPosition();
+        transform.LookAt(playerTransform.position);
+    }
 
-    // Create a quaternion based on the rotation angles
-    Quaternion rotation = Quaternion.Euler(verticalRotation, horizontalRotation, 0);
+    private Vector3 GetDesiredCamPosition()
+    {
 
-    // Use the rotation to set the camera direction
-    Vector3 camDirection = rotation * Vector3.forward;
+        float mouseX = Input.GetAxis("Mouse X") * rotationSpeedX;
+        float mouseY = -Input.GetAxis("Mouse Y") * rotationSpeedY;
 
-    // Update camera position based on player position and distance
-    transform.position = playerTransform.position + camDirection * camDistance;
-    transform.LookAt(playerTransform.position);
-}
+        // Incrementally update the rotation angles
+        currentRotationY += mouseX;
+        currentRotationX = Mathf.Clamp(currentRotationX + mouseY, -maxYRotation, -minYRotation);
+
+        // Create a quaternion based on the rotation angles
+        Quaternion rotation = Quaternion.Euler(currentRotationX, currentRotationY, 0);
+
+        // Use the rotation to set the camera direction
+        desiredCamDirection = rotation * Vector3.forward;
+
+        // Update camera position based on player position and distance
+        return playerTransform.position + desiredCamDirection * camDistance;
+    }
+
     private void CheckForPlayer()
     {
         playerTransform = GameObject.FindWithTag("Player").transform;
@@ -77,7 +88,7 @@ void Update()
     {
         if (playerTransform)
         {
-            var directionEndPoint = playerTransform.position + camDirection;
+            var directionEndPoint = playerTransform.position + desiredCamDirection;
             Gizmos.DrawLine(playerTransform.position, directionEndPoint);
         }
     }
